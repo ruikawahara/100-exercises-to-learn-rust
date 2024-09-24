9ab331226c65f5781bc044892e32f57767df92dc
Rui <ruimobile2012@gmail.com>
2024-09-24 02:51:13 -0700

    07_setters complete

--- exercises/03_ticket_v1/07_setters/src/lib.rs
+++ exercises/03_ticket_v1/07_setters/src/lib.rs
@@ -11,21 +11,25 @@ pub struct Ticket {
 
 impl Ticket {
     pub fn new(title: String, description: String, status: String) -> Ticket {
-        if title.is_empty() {
-            panic!("Title cannot be empty");
-        }
-        if title.len() > 50 {
-            panic!("Title cannot be longer than 50 bytes");
-        }
-        if description.is_empty() {
-            panic!("Description cannot be empty");
-        }
-        if description.len() > 500 {
-            panic!("Description cannot be longer than 500 bytes");
-        }
-        if status != "To-Do" && status != "In Progress" && status != "Done" {
-            panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed");
-        }
+        // if title.is_empty() {
+        //     panic!("Title cannot be empty");
+        // }
+        // if title.len() > 50 {
+        //     panic!("Title cannot be longer than 50 bytes");
+        // }
+        // if description.is_empty() {
+        //     panic!("Description cannot be empty");
+        // }
+        // if description.len() > 500 {
+        //     panic!("Description cannot be longer than 500 bytes");
+        // }
+        // if status != "To-Do" && status != "In Progress" && status != "Done" {
+        //     panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed");
+        // }
+
+        Ticket::is_valid_title(&title);
+        Ticket::is_valid_description(&description);
+        Ticket::is_valid_status(&status);
 
         Ticket {
             title,
@@ -44,6 +48,45 @@ impl Ticket {
 
     pub fn status(&self) -> &String {
         &self.status
+    }
+
+    pub fn set_title(&mut self, title: String) {
+        Ticket::is_valid_title(&title);
+        self.title = title
+    }
+
+    pub fn set_description(&mut self, description: String) {
+        Ticket::is_valid_description(&description);
+        self.description = description
+    }
+
+    pub fn set_status(&mut self, status: String) {
+        Ticket::is_valid_status(&status);
+        self.status = status
+    }
+
+    fn is_valid_title(title: &String) {
+        if title.is_empty() {
+            panic!("Title cannot be empty");
+        }
+        if title.len() > 50 {
+            panic!("Title cannot be longer than 50 bytes");
+        }
+    }
+
+    fn is_valid_description(description: &String) {
+        if description.is_empty() {
+            panic!("Description cannot be empty");
+        }
+        if description.len() > 500 {
+            panic!("Description cannot be longer than 500 bytes");
+        }
+    }
+
+    fn is_valid_status(status: &String) {
+        if status != "To-Do" && status != "In Progress" && status != "Done" {
+            panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed");
+        }
     }
 }
 
