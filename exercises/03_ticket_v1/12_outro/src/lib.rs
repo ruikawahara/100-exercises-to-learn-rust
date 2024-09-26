c46e13331e85dd001d72606b4729341f3e2e8d08
Rui <ruimobile2012@gmail.com>
2024-09-25 17:02:04 -0700

    03_tickcet_v1 complete

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -11,3 +11,77 @@
 // Integration here has a very specific meaning: they test **the public API** of your project.
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
+
+pub struct Order {
+    product_name: String,
+    quantity: u32,
+    unit_price: u32,
+}
+
+impl Order {
+    pub fn new(product_name: String, quantity: u32, unit_price: u32) -> Order {
+        Order::is_valid_product(&product_name);
+        Order::is_valid_quantity(&quantity);
+        Order::is_valid_price(&unit_price);
+
+        Order {
+            product_name,
+            quantity,
+            unit_price,
+        }
+    }
+
+    // getters
+    pub fn product_name(&self) -> &String {
+        &self.product_name
+    }
+    pub fn quantity(&self) -> &u32 {
+        &self.quantity
+    }
+    pub fn unit_price(&self) -> &u32 {
+        &self.unit_price
+    }
+
+    // setters
+    pub fn set_product_name(&mut self, product_name: String) {
+        Order::is_valid_product(&product_name);
+        self.product_name = product_name;
+    }
+
+    pub fn set_quantity(&mut self, quantity: u32) {
+        Order::is_valid_quantity(&quantity);
+        self.quantity = quantity;
+    }
+
+    pub fn set_unit_price(&mut self, unit_price: u32) {
+        Order::is_valid_price(&unit_price);
+        self.unit_price = unit_price;
+    }
+
+    // total function
+    pub fn total(&self) -> u32 {
+        self.unit_price * self.quantity
+    }
+
+    fn is_valid_product(product_name: &String) {
+        if product_name.len() < 1 {
+            panic!("Product name cannot be empty");
+        }
+
+        if product_name.len() > 300 {
+            panic!("Product name cannot be greater than 300 bytes");
+        }
+    }
+
+    fn is_valid_quantity(quantity: &u32) {
+        if *quantity < 1 {
+            panic!("Quantity must be greater than 0");
+        }
+    }
+
+    fn is_valid_price(unit_price: &u32) {
+        if *unit_price < 1 {
+            panic!("Unit price must be greater than 0");
+        }
+    }
+}
